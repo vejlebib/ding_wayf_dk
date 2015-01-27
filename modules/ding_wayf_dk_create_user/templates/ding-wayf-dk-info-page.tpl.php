d45a8c8fb54ab78764e395b62ed91127f2963a8d
Jesper Kristensen <cableman@linuxdev.dk>
2015-01-27 12:10:16 +0100

    Issue #AAKBET-5 by cableman: Fixed redirects after login

--- modules/ding_wayf_dk_create_user/templates/ding-wayf-dk-info-page.tpl.php
+++ modules/ding_wayf_dk_create_user/templates/ding-wayf-dk-info-page.tpl.php
@@ -4,6 +4,9 @@
       <h2 class="pane-title"><?php print render($title); ?></h2>
       <div class="pane-content">
         <?php print render($content); ?>
+        <div class="create-user-link">
+          <?php print render($link); ?>
+        </div>
       </div>
     </div>
   </div>
