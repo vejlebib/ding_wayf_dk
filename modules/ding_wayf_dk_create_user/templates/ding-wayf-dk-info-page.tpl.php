9244a35c1079f1982765ea089e9a4e827a317249
Jesper Kristensen <cableman@linuxdev.dk>
2015-01-28 15:18:29 +0100

    Issue #AAKBET-65 by cableman: Fixed redirects and session handling during self reg.

--- modules/ding_wayf_dk_create_user/templates/ding-wayf-dk-info-page.tpl.php
+++ modules/ding_wayf_dk_create_user/templates/ding-wayf-dk-info-page.tpl.php
@@ -1,12 +1,10 @@
-<div class="contextual-links-region">
-  <div class="left-sidebar default-layout">
-    <div class="layout-wrapper">
-      <h2 class="pane-title"><?php print render($title); ?></h2>
-      <div class="pane-content">
-        <?php print render($content); ?>
-        <div class="create-user-link">
-          <?php print render($link); ?>
-        </div>
+<div class="default-layout">
+  <div class="layout-wrapper">
+    <h2 class="pane-title"><?php print render($title); ?></h2>
+    <div class="pane-content">
+      <?php print render($content); ?>
+      <div class="create-user-link">
+        <?php print render($link); ?>
       </div>
     </div>
   </div>
